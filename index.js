1653cd00375d8746b06ba7a8ce3cddb30d4edcb9
Abhinav Garg <abhinav.garg1218@gmail.com>
2020-04-22 17:00:47 +0530

    deploy built app

--- index.js
+++ index.js
@@ -167,6 +167,15 @@ app.get('/api/parts', (req, res) =>
   })
 );
 
+// here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+
 app.post('/api/cart', (req, res) => 
   setTimeout(() => res.status(201).send(), 800)
 );
